dec363db910ff4ef76ede1066ccbcd3a79f6407f
Seika <artistnoob6@gmail.com>
2025-11-03 21:13:22 +0530

    add: Backend Completed.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const chess=new Chess();
 //settng up variables
 //
 let players={};
-let Cplayer="w";
+let currentPlayer="w";
 
 ////////////////////////////ROUTES
 
@@ -33,6 +33,66 @@ res.render("index",{title:"Chess Game"});
 io.on("connection",function(uniqueSocket){
 console.log("connected");
 
+
+//difference :between socket.emit and io.emit
+//uniquesocket is refernceing to the person who is on pc and io refers to all the players
+
+if (!players.white){
+players.white=uniqueSocket.id;
+uniqueSocket.emit("playerRole","w");
+
+}else if(!players.black){
+
+players.black=uniqueSocket.id;
+uniqueSocket.emit("playerRole","b");
+}
+else{
+uniqueSocket.emit("spectatorRole");
+}
+
+uniqueSocket.on("disconnect",()=>{
+	if (uniqueSocket.id=== players.white){
+	delete players.white;
+
+	}
+	else if (uniqueSocket.id === players.black){
+	delete players.black;
+
+	}
+
+ })
+
+
+//check if move is correct or not
+
+uniqueSocket.on("move",(move)=>{
+
+try{
+
+if(chess.turn()==='w' && uniqueSocket.id != players.white)return;
+if(chess.turn()==="b" && uniqueSocket.id != players.black)return;
+
+const result=chess.move(move);
+
+if (result){
+
+currentPlayer=chess.turn();
+io.emit("move",move);
+io.emit("boardState",chess.fn())
+}
+else{
+
+console.log("Invalid Move : ", move);
+	uniqueSocket.emit("invalidMove",move);
+}
+
+}
+catch(err){
+console.log(err);
+uniqueSocket.emit("Invalid move : ",move)
+}
+
+})
 
 //uniqueSocket.on("disconnect",()=>{
 //console.log("disconnected")
